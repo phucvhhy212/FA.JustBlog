9fd1871e3a93ae8f1d8b2799115eede0645e265e
phucvhhe161615 <phucvhhe161615@fpt.edu.vn>
2023-08-11 14:28:29 +0700

    modify connectionstring

--- FA.JustBlog.Core/Models/JustBlogContext.cs
+++ FA.JustBlog.Core/Models/JustBlogContext.cs
@@ -25,7 +25,7 @@ namespace FA.JustBlog.Core.Models
         {
             base.OnConfiguring(optionsBuilder);
             if(!optionsBuilder.IsConfigured)
-                optionsBuilder.UseSqlServer(@"server=localhost;database=BlogDb;Integrated security=true;TrustServerCertificate=true");
+                optionsBuilder.UseSqlServer(@"Server=AUG31;Database=BlogDb;User Id=sa;Password=123;TrustServerCertificate=true");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
